97f59bc436af6063fd94507042e120945729be61
Duy Minh <duyminh081201@gmail.com>
2023-01-13 16:46:26 +0700

    setup session watercolor confetti

--- src/modules/home/SectionWatercolorConfetti/index.js
+++ src/modules/home/SectionWatercolorConfetti/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import ButtonCustom from "../../../components/ButtonCustom";
+import "./SectionWatercolorConfetti.scss"
 
 const SectionWatercolorConfetti = () => {
   return (
-    <div>SectionWatercolorConfetti</div>
-  )
-}
+    <section className="SectionWatercolorConfetti">
+      <h1>Watercolor Confetti</h1>
+      <p>
+        An introductory set perfect for travel, sketching, basic watercoloring
+        and more. This set provides all the basic watercolors you need in one
+        set, offering quality and brilliance you love from Art Philosophy.
+        Included is a sturdy metal tin and mixing foldout well for easy color
+        mixing.
+      </p>
+      <ButtonCustom>Learn More</ButtonCustom>
+    </section>
+  );
+};
 
-export default SectionWatercolorConfetti
+export default SectionWatercolorConfetti;
