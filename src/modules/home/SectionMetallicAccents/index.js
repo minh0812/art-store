afc529409cc461618767b1fb9d4ef8dc84f82977
Duy Minh <duyminh081201@gmail.com>
2023-01-20 23:48:12 +0700

    Section Metallic Accents

--- src/modules/home/SectionMetallicAccents/index.js
+++ src/modules/home/SectionMetallicAccents/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import { Col, Row } from "antd";
+import React from "react";
+import "./SectionMetallicAccents.scss";
+import ButtonCustom from "../../../components/ButtonCustom";
 const SectionMetallicAccents = () => {
   return (
-    <div>SectionMetallicAccents</div>
-  )
-}
+    <section className="SectionMetallicAccents">
+      <Row>
+        <Col span={12} className="SectionMetallicAccents__left">
+          <div className="SectionMetallicAccents__left__content">
+            <h1>METALLIC ACCENTS</h1>
+            <div className="SectionWatercolorConfections__right__separator"></div>
+            <p>
+              Discover Metallic Accents! A truly vibrant and reflective
+              wate-activated pan set perfect for adding accents to your projects
+              and watercolor pieces. Simply add water to activate! Works on
+              light and dark surfaces and adds a reflective effect on most
+              surfaces.
+            </p>
+            <ButtonCustom>Learn More</ButtonCustom>
+          </div>
+        </Col>
+        <Col span={12}></Col>
+      </Row>
+    </section>
+  );
+};
 
-export default SectionMetallicAccents
+export default SectionMetallicAccents;
