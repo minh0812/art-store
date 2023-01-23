4b1719b3bc9222c1ef86fa9015664cf6c6243029
Duy Minh <duyminh081201@gmail.com>
2023-01-23 23:34:41 +0700

    Section Oil Pastels

--- src/modules/home/SectionOilPastels/index.js
+++ src/modules/home/SectionOilPastels/index.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Col, Row } from "antd";
+import React from "react";
+import "./SectionOilPastels.scss";
+import ButtonCustom from "../../../components/ButtonCustom";
 
 const SectionOilPastels = () => {
   return (
-    <div>SectionOilPastels</div>
-  )
-}
+    <section className="SectionOilPastels">
+      <Row>
+        <Col span={12}></Col>
+        <Col span={12} className="SectionOilPastels__left">
+          <div className="SectionOilPastels__left__content">
+            <h1>Oil Pastels</h1>
+            <div className="SectionWatercolorConfections__right__separator"></div>
+            <p>
+              Blend, color and layer, our Art Philosophy Oil Pastels offer a
+              higher grade of color and pigmentation for a truly beautiful
+              finish every time. Mix and blend our pastels for a customized
+              color palette. Includes a variety of colors for every artistic
+              endeavor.
+            </p>
+            <ButtonCustom>Learn More</ButtonCustom>
+          </div>
+        </Col>
+      </Row>
+    </section>
+  );
+};
 
-export default SectionOilPastels
+export default SectionOilPastels;
