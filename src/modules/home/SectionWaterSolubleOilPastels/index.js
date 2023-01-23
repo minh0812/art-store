fcca1fecd0065acbade36a557d7621c282f30dad
Duy Minh <duyminh081201@gmail.com>
2023-01-24 00:19:24 +0700

    Section WaterSoluble Oil Pastels

--- src/modules/home/SectionWaterSolubleOilPastels/index.js
+++ src/modules/home/SectionWaterSolubleOilPastels/index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./SectionWaterSolubleOilPastels.scss";
+import ButtonCustom from "../../../components/ButtonCustom";
+import { Col, Row } from "antd";
 
 const SectionWaterSolubleOilPastels = () => {
   return (
-    <div>SectionWaterSolubleOilPastels</div>
-  )
-}
+    <section className="SectionWaterSolubleOilPastels">
+      <Row>
+        <Col span={12} className="SectionWaterSolubleOilPastels__left">
+          <div className="SectionWaterSolubleOilPastels__left__content">
+            <h1>WATER SOLUBLE OIL PASTELS</h1>
+            <div className="SectionWatercolorConfections__right__separator"></div>
+            <p>
+              A perfect duo, our beautiful oil pastels with the versatility of
+              watercolor! Use our oil pastels traditionally or add water to
+              create a variety of blending techniques and gradients. Includes a
+              variety of colors for endless blending and ease of use.
+            </p>
+            <ButtonCustom>Learn More</ButtonCustom>
+          </div>
+        </Col>
+      </Row>
+    </section>
+  );
+};
 
-export default SectionWaterSolubleOilPastels
+export default SectionWaterSolubleOilPastels;
