c0dc69f47d53efbe151a32e43eda4de1b589f252
Duy Minh <duyminh081201@gmail.com>
2023-01-26 10:05:15 +0700

    add link to item

--- src/modules/home/SectionSocial/index.js
+++ src/modules/home/SectionSocial/index.js
@@ -13,18 +13,26 @@ const SectionSocial = () => {
       <h1>Contact</h1>
       <hr />
       <Space className="SectionSocial__GroupIcon">
-        <Tooltip title="Facebook" color="#1877f2">
-          <FacebookFilled className="icon facebook" />
-        </Tooltip>
-        <Tooltip title="Instagram" color="#e4405f">
-          <InstagramOutlined className="icon instagram" />
-        </Tooltip>
-        <Tooltip title="Twitter" color="#1da1f2">
-          <TwitterOutlined className="icon twitter" />
-        </Tooltip>
-        <Tooltip title="Youtube" color="#ff0000">
-          <YoutubeFilled className="icon youtube" />
-        </Tooltip>
+        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+          <Tooltip title="Facebook" color="#1877f2">
+            <FacebookFilled className="icon facebook" />
+          </Tooltip>
+        </a>
+        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+          <Tooltip title="Instagram" color="#e4405f">
+            <InstagramOutlined className="icon instagram" />
+          </Tooltip>
+        </a>
+        <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+          <Tooltip title="Twitter" color="#1da1f2">
+            <TwitterOutlined className="icon twitter" />
+          </Tooltip>
+        </a>
+        <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
+          <Tooltip title="Youtube" color="#ff0000">
+            <YoutubeFilled className="icon youtube" />
+          </Tooltip>
+        </a>
       </Space>
     </section>
   );
