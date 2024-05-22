90f737899a12b2335527204adf6c00badc609878
Duy Minh <minhpd@supertek.vn>
2024-05-22 19:31:52 +0700

    test push notice

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ import reportWebVitals from "./reportWebVitals";
 import DefaultLayout from "./layouts/DefaultLayout";
 import { ConfigProvider } from "antd";
 
-import { checkVersion, unCheckVersion } from "version-rocket";
+import { pollingCompareVersion } from "version-rocket";
 // It is recommended to use the version field in package.json, or you can customize versions
 const { version } = require("../package.json");
 
-checkVersion({
-  localPackageVersion: version,
-  originVersionFileUrl: `${window.location.origin}/version.json`,
-  // Refer to API for more configuration options
-});
-
-// To terminate version detection, call the unCheckVersion method during the destruction life cycle. For details, see the API
-unCheckVersion({ closeDialog: false });
+pollingCompareVersion(
+  version,
+  `${window.location.origin}/version.json`,
+  30000,
+  (data) => {
+    console.log(data);
+  }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
