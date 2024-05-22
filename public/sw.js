9b0a547135c0e6dd44afb52094a8c7dcf3e5e1e0
Duy Minh <minhpd@supertek.vn>
2024-05-22 17:54:24 +0700

    change sevice worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,6 @@
 // sw.js
+const cacheName = 'cache-v1'; 
+
 self.addEventListener("install", function (event) {
   self.skipWaiting();
 });
@@ -17,22 +19,42 @@ self.addEventListener("message", function (event) {
   }
 });
 
-// Kiểm tra xem có bản cập nhật mới không
-self.addEventListener("install", function (event) {
+// Gọi sự kiện Install
+self.addEventListener('install', (e) => {
   self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      // Thêm các tệp vào bộ nhớ cache
+      return cache.addAll([
+        '/index.html',
+        '/main.js',
+        '/style.css',
+      ]);
+    })
+  );
 });
 
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    clients.matchAll().then(function (clients) {
-      clients.forEach(function (client) {
-        // Gửi thông báo cho người dùng
-        client.postMessage({
-          title: "Cập nhật",
-          message: "Ứng dụng đã được cập nhật. Vui lòng tải lại trang.",
-          action: "reload",
-        });
-      });
+// Gọi sự kiện Activate để dọn dẹp các bộ nhớ cache cũ và thông báo cho người dùng
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            // Nếu có bản cập nhật mới, gửi thông báo cho người dùng
+            self.clients.matchAll().then(function(clients) {
+              clients.forEach(function(client) {
+                client.postMessage({ 
+                  title: 'Cập nhật',
+                  message: 'Ứng dụng đã được cập nhật. Vui lòng tải lại trang.',
+                  action: 'reload'
+                });
+              });
+            });
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
