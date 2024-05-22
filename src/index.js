70c1a97986979748dd36dae98f8fccd0558c9f1c
Duy Minh <minhpd@supertek.vn>
2024-05-22 19:46:13 +0700

    test push notice

--- src/index.js
+++ src/index.js
@@ -8,9 +8,22 @@ import reportWebVitals from "./reportWebVitals";
 import DefaultLayout from "./layouts/DefaultLayout";
 import { ConfigProvider } from "antd";
 
-import { checkVersion, unCheckVersion } from "version-rocket";
+import {
+  checkVersion,
+  pollingCompareVersion,
+  unCheckVersion,
+} from "version-rocket";
 // It is recommended to use the version field in package.json, or you can customize versions
 const { version } = require("../package.json");
+
+pollingCompareVersion(
+  version,
+  `${window.location.origin}/version.json`,
+  30000,
+  (data) => {
+    console.log(data);
+  }
+);
 
 checkVersion({
   localPackageVersion: version,
